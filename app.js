0df5e4360f0a9771403fa875d9932f6f22713a26
unknown <nayakpunya43@gmail.com>
2021-03-12 17:58:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var outputResult = document.querySelector("#output")
+
+function afunction(){
+    var btnAvalue = document.getElementById("btn1").value;
+    console.log(btnAvalue);
+    outputResult.innerHTML = btnAvalue;
+}
+function bfunction(){
+    var btnBvalue = document.getElementById("btn2").value;
+    outputResult.innerHTML = btnBvalue;
+}
+function cfunction(){
+    var btnCvalue = document.getElementById("btn3").value;
+    outputResult.innerHTML = btnCvalue;
+}
+function dfunction(){
+    var btnDvalue = document.getElementById("btn4").value;
+    outputResult.innerHTML = btnDvalue;
+}
+function efunction(){
+    var btnEvalue = document.getElementById("btn5").value;
+    outputResult.innerHTML = btnEvalue;
+}
+function ffunction(){
+    var btnFvalue = document.getElementById("btn6").value;
+    outputResult.innerHTML = btnFvalue;
+}
+function gfunction(){
+    var btnGvalue = document.getElementById("btn7").value;
+    outputResult.innerHTML = btnGvalue;
+}
+function hfunction(){
+    var btnHvalue = document.getElementById("btn8").value;
+    outputResult.innerHTML = btnHvalue;
+}
+function ifunction(){
+    var btnIvalue = document.getElementById("btn9").value;
+    outputResult.innerHTML = btnIvalue;
+}
+function jfunction(){
+    var btnJvalue = document.getElementById("btn10").value;
+    outputResult.innerHTML = btnJvalue;
+}
+
+
